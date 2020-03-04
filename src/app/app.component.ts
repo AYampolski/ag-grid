682f533b212c2ec26837aa6fa92b0d828b7f466a
Alexander Yampolski <al.yampolski@gmail.com>
2020-03-05 01:24:40 +0200

    Add context menu for title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonService } from '@services/common.service';
+import { AllModules, Module } from '@ag-grid-enterprise/all-modules';
 import { ImageFormatterComponent } from '@components/image-formatter/image-formatter.component';
 
 @Component({
@@ -10,6 +11,7 @@ import { ImageFormatterComponent } from '@components/image-formatter/image-forma
 export class AppComponent {
   title = 'testGrid';
   rowData;
+  public modules: Module[] = AllModules;
   getRowHeight;
   private gridApi;
   private gridColumnApi;
@@ -35,5 +37,21 @@ export class AppComponent {
       this.rowData = data;
       console.log(data);
     });
+  }
+
+  getContextMenuItems(params) {
+    if (params.column.colDef.headerName !== 'Video Title') {
+      return;
+    }
+    const result = [
+      {
+        name: 'Open in new tab ' + params.column.colDef.headerName,
+        action: function() {
+          window.open(params.value, '_blank');
+        },
+        cssClasses: ['redFont', 'bold'],
+      },
+    ];
+    return result;
   }
 }
