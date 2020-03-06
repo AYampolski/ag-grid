4c8d7e7ce2b9f261612f07cd2ea4d368db6541a7
Alexander Yampolski <al.yampolski@gmail.com>
2020-03-06 09:33:45 +0200

    Remove specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,47 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+import { ImageFormatterComponent } from './components/image-formatter/image-formatter.component';
+import { ModeToolComponent } from './components/mode-tool/mode-tool.component';
+import { TotalToolComponent } from './components/total-tool/total-tool.component';
+import { SelectedToolComponent } from './components/selected-tool/selected-tool.component';
+import { SelectedCellRenderComponent } from './components/selected-cell-render/selected-cell-render.component';
+import { HeaderCustomComponent } from './components/header-custom/header-custom.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule, AgGridModule.withComponents([
+        ImageFormatterComponent,
+        ModeToolComponent,
+        TotalToolComponent,
+        SelectedToolComponent,
+        SelectedCellRenderComponent,
+        HeaderCustomComponent
+      ])],
       declarations: [
-        AppComponent
+        AppComponent,
+        ImageFormatterComponent,
+        ModeToolComponent,
+        TotalToolComponent,
+        SelectedToolComponent,
+        SelectedCellRenderComponent,
+        HeaderCustomComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it('grid API is not available until  `detectChanges`', () => {
+    expect(component.gridOptions.api).not.toBeTruthy();
+});
 
-  it(`should have as title 'testGrid'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testGrid');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testGrid app is running!');
-  });
 });
